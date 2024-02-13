4397270a737da5640f90bd485292f9326b32ede6
wl <mischudrag@gmail.com>
2024-02-13 14:41:15 +0200

    modificat

--- src/server.js
+++ src/server.js
@@ -1,33 +1,36 @@
 const express = require('express');
-const axios = require('axios');
 
 let app = express();
 app.use(express.static(__dirname + '/frontend'))
 app.set('views', __dirname + '/frontend');
 app.set('view engine', 'ejs');
 
-let IP = 'http://192.168.68.114'
+let IP = 'http://192.168.68.110'
+const PORT = 3000;
 
 app.get('/', async (req, res) => {
     let data;
-    await axios.get(`${IP}/solar_api/v1/GetPowerFlowRealtimeData.fcgi`)
-            .then(response => {
-                data = {
-                    production: response.data.Body.Data.Inverters['1'].P,
-                    grid: Math.floor(response.data.Body.Data.Site.P_Grid)
-                }
-            })
-            .catch(error => {
-                console.log(error)
-            })
+    
+    await fetch(`${IP}/solar_api/v1/GetPowerFlowRealtimeData.fcgi`)
+        .then(response => response.json())
+        .then(response => {
+            data = {
+                production: response.Body.Data.Inverters['1'].P,
+                grid: Math.floor(response.Body.Data.Site.P_Grid)
+            }
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    
     res.render('index', {
         titlu: 'Solar ☀️',
         production: data.production,
-        consumption: data.production + data.grid,
+        consumption: (data.production + data.grid),
         grid: data.grid
     });
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Server is running on port 3000');
 });
